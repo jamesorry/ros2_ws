f12c327b13b508ba2b09316dda8b8323a496d599
jamesorry <jamesnail333@gmail.com>
2022-07-03 03:23:06 +0800

    add BT ros2

---
+++ src/BT_ros2/README.md
@@ -0,0 +1,152 @@
+# BT on ROS 2
+
+BT_ros2 is a demo of how to use Behavior Tree to control AMR.
+All the examples are tested on our NeuronBot simulation. If you are interested in NeuronBot simulation, please go to see the tutorials: https://github.com/Adlink-ROS/neuronbot2#bring-up-in-simulation
+
+# Build
+1. git clone the repo.
+```bash
+mkdir -p ~/bt_ros2_ws/src
+cd ~/bt_ros2_ws/src
+git clone https://github.com/Adlink-ROS/BT_ros2.git
+```
+2. Install dependencies
+```bash
+source /opt/ros/<ROS2_DISTRO>/setup.bash
+cd ~/bt_ros2_ws
+rosdep install --from-paths src --ignore-src -r -y
+```
+3. Build
+```bash
+colcon build --symlink-install
+# If you want to build with OpenVINO support, add the define
+# Remember to source OpenVINO before build
+colcon build --symlink-install --cmake-args -DBUILD_OPENVINO=ON
+```
+
+# Usage
+
+We will run Gazebo with NeuronBot2 and show simple BT examples.
+
+## Basic Example
+
+This example makes NeuronBot2 continuously moving between the pre-defined three goals in ```bt_nav_mememan.xml```.
+
+* Open 1st terminal and run mememan world.
+```bash
+source /opt/ros/<ROS2_DISTRO>/local_setup.bash
+source ~/neuronbot2_ros2_ws/install/local_setup.bash
+ros2 launch neuronbot2_gazebo neuronbot2_world.launch.py world_model:=mememan_world.model
+```
+* Open 2nd terminal and run navigation.
+```bash
+source /opt/ros/<ROS2_DISTRO>/local_setup.bash
+source ~/neuronbot2_ros2_ws/install/local_setup.bash
+ros2 launch neuronbot2_nav bringup_launch.py map:=$HOME/neuronbot2_ros2_ws/src/neuronbot2/neuronbot2_nav/map/mememan.yaml open_rviz:=true use_sim_time:=true
+```
+* Open 3rd termainal and run BT.
+```bash
+source /opt/ros/<ROS2_DISTRO>/local_setup.bash
+source ~/bt_ros2_ws/install/local_setup.bash
+ros2 launch bt_ros2 bt_ros2.launch.py
+```
+
+## Advanced Example - Interrupt
+
+In this example (refer to ```bt_nav_mememan_interrupt.xml```), when BT receives /interrupt_event, which is gohome, then NeuronBot2 will move to Goal_c.
+
+* Open 1st terminal and run mememan world.
+```bash
+source /opt/ros/<ROS2_DISTRO>/local_setup.bash
+source ~/neuronbot2_ros2_ws/install/local_setup.bash
+ros2 launch neuronbot2_gazebo neuronbot2_world.launch.py world_model:=mememan_world.model
+```
+* Open 2nd terminal and run navigation.
+```bash
+source /opt/ros/<ROS2_DISTRO>/local_setup.bash
+source ~/neuronbot2_ros2_ws/install/local_setup.bash
+ros2 launch neuronbot2_nav bringup_launch.py map:=$HOME/neuronbot2_ros2_ws/src/neuronbot2/neuronbot2_nav/map/mememan.yaml open_rviz:=true use_sim_time:=true
+```
+* Open 3rd termainal and run BT.
+```bash
+source /opt/ros/<ROS2_DISTRO>/local_setup.bash
+source ~/bt_ros2_ws/install/local_setup.bash
+ros2 run bt_ros2 bt_ros2 --ros-args -p bt_xml:=$HOME/bt_ros2_ws/src/BT_ros2/bt_xml/bt_nav_mememan_interrupt.xml
+```
+* Open 4th terminal and pub interrupt event.
+```bash
+source /opt/ros/<ROS2_DISTRO>/local_setup.bash
+ros2 topic pub -1 /interrupt_event std_msgs/msg/String data:\ \'gohome\'
+```
+
+## Advanced Example - Take a snapshot
+
+The demo is to show how to take snapshots with BT example.
+You need to install OpenVINO and its ROS 2 wrapper before using the example.
+
+* Open 1st terminal and run OpenVINO object detection
+```bash
+# Source ROS 2 environment
+# Setup OpenVINO environment and source wrapper
+ros2 launch dynamic_vino_sample pipeline_object.launch.py
+```
+* Open 2nd terminal and run image_saver
+  - If you don't have image_view, please run `sudo apt install ros-${ROS_DISTRO}-image-view`
+```bash
+# Source ROS 2 environment
+ros2 run image_view image_saver image:=ros2_openvino_toolkit/image_rviz
+```
+* Open 3rd terminal and run BT
+```bash
+# Source ROS 2 environment
+source ~/bt_ros2_ws/install/local_setup.bash
+ros2 run bt_ros2 bt_ros2 --ros-args -p bt_xml:=$HOME/bt_ros2_ws/src/BT_ros2/bt_xml/bt_snapshot.xml
+```
+
+## Advanced Example - OpenVINO object detection
+
+This demo shows an integrated application with object detection and robot navigation.
+With different signal triggered by OpenVINO, the robot will navigate to certain position.
+The objects in the BT file are animals, which are cat, dog, cow.
+
+* Open 1st terminal and run mememan world.
+```bash
+# Source ROS 2 environment
+source ~/neuronbot2_ros2_ws/install/local_setup.bash
+ros2 launch neuronbot2_gazebo neuronbot2_world.launch.py world_model:=mememan_world.model
+```
+* Open 2nd terminal and run OpenVINO object detection
+```bash
+# Source ROS 2 environment
+# Setup OpenVINO environment and source wrapper
+ros2 launch dynamic_vino_sample pipeline_object.launch.py
+```
+* Open 3rd terminal and run navigation.
+```bash
+# Source ROS 2 environment
+source ~/neuronbot2_ros2_ws/install/local_setup.bash
+ros2 launch neuronbot2_nav bringup_launch.py map:=$HOME/neuronbot2_ros2_ws/src/neuronbot2/neuronbot2_nav/map/mememan.yaml open_rviz:=true use_sim_time:=true
+```
+* Open 4th terminal and run BT
+```bash
+# Source ROS 2 environment
+# Setup OpenVINO environment and source wrapper
+source ~/bt_ros2_ws/install/local_setup.bash
+ros2 run bt_ros2 bt_ros2 --ros-args -p bt_xml:=$HOME/bt_ros2_ws/src/BT_ros2/bt_xml/bt_openvino.xml
+# An additional bt_xml file is prepared, which switch the action after trigger from navigation to teleop command.
+#ros2 run bt_ros2 bt_ros2 --ros-args -p bt_xml:=$HOME/bt_ros2_ws/src/BT_ros2/bt_xml/bt_openvino_teleop.xml
+```
+
+# Note
+
+If you want to get the coordinate for navigation, you can run navigation2 and listen to the topic /goal_pose or open rviz2 to monitor tf.
+
+The position and orientation should be put into BT file and the orientation value we use here is Quaternion.
+
+Record X, Y in positoin and Z, W in orientation.
+![](readme_resource/inspect_rviz.png)
+
+Open xml file in bt_xml and modify robot checkpoint with " X ; Y ; Z ; W " format in SetBlackboard.
+![](readme_resource/bt_modify.png)
+
+If you only have Euler Angle and want to transfer to Quaternion, please refer to online tools, such as https://quaternions.online/
--- src/yolobot/yolobot_recognition/scripts/ros_recognition_yolo.py
+++ src/yolobot/yolobot_recognition/scripts/ros_recognition_yolo.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python
-import os, sys
+#!/usr/bin/env python3
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+from utils.plots import colors, plot_one_box
+from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
+    apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
+from utils.augmentations import Albumentations, augment_hsv, copy_paste, letterbox, mixup, random_perspective
+from utils.datasets import LoadStreams, LoadImages
+from models.experimental import attempt_load
+import os
+import sys
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import Header
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
@@ -13,44 +22,40 @@ from pathlib import Path
 import torch
 import torch.backends.cudnn as cudnn
 
+from my_robot_interfaces.msg import BoundingBoxes, BoundingBox
+
 FILE = Path(__file__).absolute()
 sys.path.append(FILE.parents[0].as_posix())
 
-from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.augmentations import Albumentations, augment_hsv, copy_paste, letterbox, mixup, random_perspective
-from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
-    apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 bridge = CvBridge()
+
 
 class Camera_subscriber(Node):
 
     def __init__(self):
         super().__init__('camera_subscriber')
 
-        weights='yolov5s.pt'  # model.pt path(s)
-        self.imgsz=640  # inference size (pixels)
-        self.conf_thres=0.25  # confidence threshold
-        self.iou_thres=0.45  # NMS IOU threshold
-        self.max_det=1000  # maximum detections per image
-        self.classes=None  # filter by class: --class 0, or --class 0 2 3
-        self.agnostic_nms=False  # class-agnostic NMS
-        self.augment=False  # augmented inference
-        self.visualize=False  # visualize features
-        self.line_thickness=3  # bounding box thickness (pixels)
-        self.hide_labels=False  # hide labels
-        self.hide_conf=False  # hide confidences
-        self.half=False  # use FP16 half-precision inference
+        weights = 'yolov5s.pt'  # model.pt path(s)
+        self.imgsz = 640  # inference size (pixels)
+        self.conf_thres = 0.25  # confidence threshold
+        self.iou_thres = 0.45  # NMS IOU threshold
+        self.max_det = 1000  # maximum detections per image
+        self.classes = None  # filter by class: --class 0, or --class 0 2 3
+        self.agnostic_nms = False  # class-agnostic NMS
+        self.augment = False  # augmented inference
+        self.visualize = False  # visualize features
+        self.line_thickness = 1  # bounding box thickness (pixels)
+        self.hide_labels = False  # hide labels
+        self.hide_conf = False  # hide confidences
+        self.half = False  # use FP16 half-precision inference
         self.stride = 32
-        device_num=''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False  # show results
-        save_crop=False  # save cropped prediction boxes
-        nosave=False  # do not save images/videos
-        update=False  # update all models
-        name='exp'  # save results to project/name
+        device_num = ''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img = False  # show results
+        save_crop = False  # save cropped prediction boxes
+        nosave = False  # do not save images/videos
+        update = False  # update all models
+        name = 'exp'  # save results to project/name
 
         # Initialize
         set_logging()
@@ -58,10 +63,12 @@ class Camera_subscriber(Node):
         self.half &= self.device.type != 'cpu'  # half precision only supported on CUDA
 
         # Load model
-        self.model = attempt_load(weights, map_location=self.device)  # load FP32 model
+        self.model = attempt_load(
+            weights, map_location=self.device)  # load FP32 model
         stride = int(self.model.stride.max())  # model stride
         imgsz = check_img_size(self.imgsz, s=stride)  # check image size
-        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names  # get class names
+        self.names = self.model.module.names if hasattr(
+            self.model, 'module') else self.model.names  # get class names
         if self.half:
             self.model.half()  # to FP16
 
@@ -69,7 +76,8 @@ class Camera_subscriber(Node):
         self.classify = False
         if self.classify:
             self.modelc = load_classifier(name='resnet50', n=2)  # initialize
-            self.modelc.load_state_dict(torch.load('resnet50.pt', map_location=self.device)['model']).to(self.device).eval()
+            self.modelc.load_state_dict(torch.load('resnet50.pt', map_location=self.device)[
+                                        'model']).to(self.device).eval()
 
         # Dataloader
         view_img = check_imshow()
@@ -77,7 +85,14 @@ class Camera_subscriber(Node):
 
         # Run inference
         if self.device.type != 'cpu':
-            self.model(torch.zeros(1, 3, imgsz, imgsz).to(self.device).type_as(next(self.model.parameters())))  # run once
+            self.model(torch.zeros(1, 3, imgsz, imgsz).to(self.device).type_as(
+                next(self.model.parameters())))  # run once
+
+        self.publish_bbox = self.create_publisher(
+            BoundingBoxes, '/robot1/yolov5/bounding_boxes', 10)
+
+        self.publish_image = self.create_publisher(
+            Image, '/robot1/yolov5/image_raw', 10)
 
         self.subscription = self.create_subscription(
             Image,
@@ -87,25 +102,53 @@ class Camera_subscriber(Node):
             10)
         self.subscription  # prevent unused variable warning
 
-    def camera_callback(self, data):
+    def yolovFive2bboxes_msgs(self, bboxes: list, scores: list, cls: list, img_header: Header):
+        bboxes_msg = BoundingBoxes()
+        bboxes_msg.header = img_header
+        # print(bboxes)
+        # print(bbox[0][0])
+        i = 0
+        for score in scores:
+            one_box = BoundingBox()
+            one_box.xmin = int(bboxes[0][i])
+            one_box.ymin = int(bboxes[1][i])
+            one_box.xmax = int(bboxes[2][i])
+            one_box.ymax = int(bboxes[3][i])
+            one_box.probability = float(score)
+            one_box.class_id = cls[i]
+            bboxes_msg.bounding_boxes.append(one_box)
+            i = i+1
+        self.publish_bbox.publish(bboxes_msg)
+
+    def camera_callback(self, data: Image):
+        class_list = []
+        confidence_list = []
+        x_min_list = []
+        y_min_list = []
+        x_max_list = []
+        y_max_list = []
+
         t0 = time.time()
         img = bridge.imgmsg_to_cv2(data, "bgr8")
 
         # check for common shapes
-        s = np.stack([letterbox(x, self.imgsz, stride=self.stride)[0].shape for x in img], 0)  # shapes
-        self.rect = np.unique(s, axis=0).shape[0] == 1  # rect inference if all shapes equal
+        s = np.stack([letterbox(x, self.imgsz, stride=self.stride)[
+                     0].shape for x in img], 0)  # shapes
+        # rect inference if all shapes equal
+        self.rect = np.unique(s, axis=0).shape[0] == 1
         if not self.rect:
             print('WARNING: Different stream shapes detected. For optimal performance supply similarly-shaped streams.')
 
         # Letterbox
         img0 = img.copy()
-        img = img[np.newaxis, :, :, :]        
+        img = img[np.newaxis, :, :, :]
 
         # Stack
         img = np.stack(img, 0)
 
         # Convert
-        img = img[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW
+        # BGR to RGB, BHWC to BCHW
+        img = img[..., ::-1].transpose((0, 3, 1, 2))
         img = np.ascontiguousarray(img)
 
         img = torch.from_numpy(img).to(self.device)
@@ -117,11 +160,12 @@ class Camera_subscriber(Node):
         # Inference
         t1 = time_synchronized()
         pred = self.model(img,
-                     augment=self.augment,
-                     visualize=increment_path(save_dir / 'features', mkdir=True) if self.visualize else False)[0]
+                          augment=self.augment,
+                          visualize=increment_path(save_dir / 'features', mkdir=True) if self.visualize else False)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, self.classes, self.agnostic_nms, max_det=self.max_det)
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres,
+                                   self.classes, self.agnostic_nms, max_det=self.max_det)
         t2 = time_synchronized()
 
         # Apply Classifier
@@ -135,24 +179,55 @@ class Camera_subscriber(Node):
 
             if len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+                det[:, :4] = scale_coords(
+                    img.shape[2:], det[:, :4], img0.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
-                
+                    # add to string
+                    s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "
+
                 for *xyxy, conf, cls in reversed(det):
                     c = int(cls)  # integer class
-                    label = None if self.hide_labels else (self.names[c] if self.hide_conf else f'{self.names[c]} {conf:.2f}')
-                    plot_one_box(xyxy, img0, label=label, color=colors(c, True), line_thickness=self.line_thickness)
+                    label = None if self.hide_labels else (
+                        self.names[c] if self.hide_conf else f'{self.names[c]} {conf:.2f}')
+                    plot_one_box(xyxy, img0, label=label, color=colors(
+                        c, True), line_thickness=self.line_thickness)
+
+                    class_list.append(self.names[c])
+                    confidence_list.append(conf)
+                    # tensor to float
+                    x_min_list.append(xyxy[0].item())
+                    y_min_list.append(xyxy[1].item())
+                    x_max_list.append(xyxy[2].item())
+                    y_max_list.append(xyxy[3].item())
+                    # print("integer class: ", c, "; lable: ", label)
+                    # print("--------------------")
 
         cv2.imshow("IMAGE", img0)
-        cv2.waitKey(4)    
+        if len(class_list):
+            renamed_list = self.rename_duplicate(class_list)
+            self.yolovFive2bboxes_msgs(bboxes=[x_min_list, y_min_list, x_max_list, y_max_list],
+                                       scores=confidence_list, cls=renamed_list, img_header=data.header)
+            # self.publish_image.publish(img0)
+
+        cv2.waitKey(4)
+
+    def rename_duplicate(self, list):
+        counts = {}
+        for index, key in enumerate(list):
+            if key in counts:
+                counts[key] += 1
+                list[index] = f'{key}_{counts[key]}'
+            else:
+                counts[key] = 0
+        # print("Renamed list:", list)
+        return list
+
 
 if __name__ == '__main__':
     rclpy.init(args=None)
     camera_subscriber = Camera_subscriber()
     rclpy.spin(camera_subscriber)
     rclpy.shutdown()
-
