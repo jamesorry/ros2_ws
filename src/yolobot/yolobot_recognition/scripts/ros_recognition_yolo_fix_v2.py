eb0e4bc0b814e71bdda7a24f55a28f59463c66c7
jamesorry <jamesnail333@gmail.com>
2022-10-09 19:50:13 +0800

    新增yolo流程圖,註解

--- src/yolobot/yolobot_recognition/scripts/ros_recognition_yolo_fix_v2.py
+++ src/yolobot/yolobot_recognition/scripts/ros_recognition_yolo_fix_v2.py
@@ -41,21 +41,25 @@ class Camera_subscriber(Node):
     def __init__(self):
         super().__init__('camera_subscriber')
 
-        weights = 'yolov5s.pt'  # model.pt path(s)
-        self.imgsz = 640  # inference size (pixels)
-        self.conf_thres = 0.25  # confidence threshold
-        self.iou_thres = 0.45  # NMS IOU threshold
-        self.max_det = 1000  # maximum detections per image
-        self.classes = None  # filter by class: --class 0, or --class 0 2 3
-        self.agnostic_nms = False  # class-agnostic NMS
+        # model.pt path(s) # 事先训练完成的权重文件，比如yolov5s.pt(官方事先訓練好的文件)
+        weights = 'yolov5s.pt'
+        # inference size (pixels) # 预测时的放缩后图片大小(因为YOLO算法需要预先放缩图片), 两个值分别是height, width
+        self.imgsz = 640
+        self.conf_thres = 0.25  # confidence threshold # 置信度阈值, 高于此值的bounding_box才会被保留
+        self.iou_thres = 0.45  # NMS IOU threshold # IOU阈值,高于此值的bounding_box才会被保留
+        self.max_det = 1000  # maximum detections per image # 一张图片上检测的最大目标数量
+        self.classes = None  # filter by class: --class 0, or --class 0 2 3 # 过滤指定类的预测结果
+        # class-agnostic NMS # 如为True,则为class-agnostic. 否则为class-specific
+        self.agnostic_nms = False
         self.augment = False  # augmented inference
         self.visualize = False  # visualize features
-        self.line_thickness = 1  # bounding box thickness (pixels)
-        self.hide_labels = False  # hide labels
-        self.hide_conf = False  # hide confidences
-        self.half = False  # use FP16 half-precision inference
-        self.stride = 32
-        device_num = ''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        # bounding box thickness (pixels) 绘制Bounding_box的线宽度
+        self.line_thickness = 1
+        self.hide_labels = False  # hide labels 隐藏标签
+        self.hide_conf = False  # hide confidences 隐藏置信度
+        self.half = False  # use FP16 half-precision inference 是否使用半精度推理（节约显存）
+        self.stride = 32 #推理时所用到的步长，默认为32， 大步长适合于大目标，小步长适合于小目标
+        device_num = ''  # cuda device, i.e. 0 or 0,1,2,3 or cpu # 所使用的GPU编号，如果使用CPU就写cpu
         view_img = False  # show results
         save_crop = False  # save cropped prediction boxes
         nosave = False  # do not save images/videos
@@ -72,11 +76,18 @@ class Camera_subscriber(Node):
         # 加載float32模型，確保輸入圖片分辨率能整除32(如果不能整除則調整成可以整除並返回)
         self.model = attempt_load(
             weights, map_location=self.device)  # load FP32 model
-        stride = int(self.model.stride.max())  # model stride
+        stride = int(self.model.stride.max())  # model stride 推理时所用到的步长
         imgsz = check_img_size(self.imgsz, s=stride)  # check image size
+        # 将图片大小调整为步长的整数倍
+        # 比如假如步长是10，imagesz是[100,101],则返回值是[100,100]
+        
         # 獲取類別名字字符串列表
         self.names = self.model.module.names if hasattr(
             self.model, 'module') else self.model.names  # get class names
+        
+        # print("self.names: ", self.names)
+        # ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
         if self.half:
             self.model.half()  # to FP16
 
@@ -88,9 +99,11 @@ class Camera_subscriber(Node):
             self.modelc.load_state_dict(torch.load('resnet50.pt', map_location=self.device)[
                                         'model']).to(self.device).eval()
 
+        # print("self.classify", self.classify)
+        
         # Dataloader
-        view_img = check_imshow() # Check if environment supports image displays
-        cudnn.benchmark = True  # set True to speed up constant image size inference
+        view_img = check_imshow()  # Check if environment supports image displays # 检测cv2.imshow()方法是否可以执行，不能执行则抛出异常
+        cudnn.benchmark = True  # set True to speed up constant image size inference # 该设置可以加速预测
 
         # Run inference
         # 進行一次向前推理，測試程序是否正常
@@ -121,7 +134,7 @@ class Camera_subscriber(Node):
         self.z_list = []
         self.subscriber_ = self.create_subscription(
             PointCloud2, "/robot1/intel_realsense_r200_depth/points", self.callback_pointcloud_XY_Plot, 10)
-        
+
     def callback_pointcloud_XY_Plot(self, data):
         assert isinstance(data, PointCloud2)
         gen = point_cloud2.read_points(
@@ -138,7 +151,7 @@ class Camera_subscriber(Node):
                     self.x_list_fix.append(x)
                     self.y_list_fix.append(y)
                     self.z_list.append(z)
-        
+
         if len(self.z_list) != 0:
             self._z_finial_disrance = statistics.mean(self.z_list)
         # print('Z list mean value(m): ', self._z_finial_disrance) # 最後取平均值 得到z軸的距離
@@ -149,7 +162,7 @@ class Camera_subscriber(Node):
         self.x_list.clear()
         self.y_list.clear()
         self.z_list.clear()
-        
+
     def yolovFive2bboxes_msgs(self, bboxes: list, scores: list, cls: list, img_header: Header):
         bboxes_msg = BoundingBoxes()
         bboxes_msg.header = img_header
@@ -162,8 +175,10 @@ class Camera_subscriber(Node):
             one_box.ymin = int(bboxes[1][i])
             one_box.xmax = int(bboxes[2][i])
             one_box.ymax = int(bboxes[3][i])
-            one_box.x_center = abs(one_box.xmax - one_box.xmin) / 2 + one_box.xmin
-            one_box.y_center = abs(one_box.ymax - one_box.ymin) / 2 + one_box.ymin
+            one_box.x_center = abs(
+                one_box.xmax - one_box.xmin) / 2 + one_box.xmin
+            one_box.y_center = abs(
+                one_box.ymax - one_box.ymin) / 2 + one_box.ymin
             one_box.probability = float(score)
             one_box.class_id = cls[i]
             one_box.center_dist = self._z_finial_disrance
@@ -189,7 +204,8 @@ class Camera_subscriber(Node):
         # img = bridge.imgmsg_to_cv2(data, "32FC1")
 
         # check for common shapes
-        s = np.stack([letterbox(x, self.imgsz, stride=self.stride)[0].shape for x in img], 0)  # shapes
+        s = np.stack([letterbox(x, self.imgsz, stride=self.stride)[
+                     0].shape for x in img], 0)  # shapes
         # rect inference if all shapes equal
         self.rect = np.unique(s, axis=0).shape[0] == 1
         if not self.rect:
@@ -207,32 +223,51 @@ class Camera_subscriber(Node):
         img = img[..., ::-1].transpose((0, 3, 1, 2))
         img = np.ascontiguousarray(img)
 
-        img = torch.from_numpy(img).to(self.device)
-        img = img.half() if self.half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        img = torch.from_numpy(img).to(self.device) #将图片放到指定设备(如GPU)上识别
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32 # 把输入从整型转化为半精度/全精度浮点数。
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0 #将图片归一化处理（这是图像表示方法的的规范，使用浮点数就要归一化）
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
 
         save_dir = '/home/james/ros2_ws/src/yolobot/yolobot_recognition/scripts'
         # Inference
         t1 = time_synchronized()
+        # visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+        # 如果为True则保留推理过程中的特征图，保存在runs文件夹中
         pred = self.model(img,
                           augment=self.augment,
                           visualize=increment_path(save_dir / 'features', mkdir=True) if self.visualize else False)[0]
-
+        # 推理结果，pred保存的是所有的bound_box的信息，
+        """ 
+        pred[..., 0:4]为预测框坐标
+        预测框坐标为xywh(中心点+宽长)格式
+        pred[..., 4]为objectness置信度
+        pred[..., 5:-1]为分类结果 
+        """
         # Apply NMS
         pred = non_max_suppression(pred, self.conf_thres, self.iou_thres,
                                    self.classes, self.agnostic_nms, max_det=self.max_det)
+        # 执行非极大值抑制，返回值为过滤后的预测框
+        # conf_thres： 置信度阈值
+        # iou_thres： iou阈值
+        # classes: 需要过滤的类（数字列表）保留特定的类别
+        # agnostic_nms： 标记class-agnostic或者使用class-specific方式。默认为class-agnostic 进行nms是否也去除不同类别之间的框
+        # max_det: 检测框结果的最大数量
+        """
+        经过nms之后,预测框格式:xywh-->xyxy(左上角右下角)
+        pred是一个列表list[torch.tensor],长度为batch_size
+        每一个torch.tensor的shape为(num_boxes, 6),内容为box+conf+cls
+        """
         t2 = time_synchronized()
 
-        # Apply Classifier
+        # Apply Classifier 預設不處理
         if self.classify:
             pred = apply_classifier(pred, self.modelc, img, img0)
 
         # Process detections
         # 對每張圖進行處理
         for i, det in enumerate(pred):  # detections per image
-            # 設置印出訊息（圖片寬高）
+            # 設置印出訊息（圖片寬高）# 显示推理前裁剪后的图像尺寸
             s = f'{i}: '
             s += '%gx%g ' % img.shape[2:]  # print string
 
@@ -240,6 +275,7 @@ class Camera_subscriber(Node):
                 # Rescale boxes from img_size to im0 size
                 # 調整預測框座標，基於resize+pad的圖片座標-->基於源size圖片的座標
                 # 此時座標格式為xyxy
+                # 将标注的bounding_box大小调整为和原图一致（因为训练时原图经过了放缩）
                 det[:, :4] = scale_coords(
                     img.shape[2:], det[:, :4], img0.shape).round()
 
@@ -275,9 +311,9 @@ class Camera_subscriber(Node):
             renamed_list = self.rename_duplicate(class_list)
             self.yolovFive2bboxes_msgs(bboxes=[x_min_list, y_min_list, x_max_list, y_max_list],
                                        scores=confidence_list, cls=renamed_list, img_header=data.header)
-            
+
         self.publish_image.publish(bridge.cv2_to_imgmsg(img0, "bgr8"))
-        
+
         cv2.waitKey(1)
 
     def rename_duplicate(self, list):
